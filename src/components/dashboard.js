efe8fa9089358a5c2e2b6ba2636cb000a9cdad3b
dbaranoff <cps.baranoff@gmail.com>
2022-07-17 17:01:13 +0300

    Merge conflicts resolved and improved

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,46 +2,39 @@ import { h } from 'preact';
 import AmmoTable from './shared/AmmoTable';
 
 const mock = [
-	{
-		category: 'Towed Artillery',
-		values: [
-			{ value: 126, delta: 1, sources: ['http://example.org/1'] },
-			{ value: 69 }
-		]
-	},
-	{
-		category: 'Self-Propelled Artillery',
-		values: [
-			{},
-			{ value: 215, delta: 4, sources: ['http://example.org/2', 'http://example.org/3'] }
-		]
-	}
+  {
+    category: 'Towed Artillery',
+    values: [{ value: 126, delta: 1, sources: ['http://example.org/1'] }, { value: 69 }],
+  },
+  {
+    category: 'Self-Propelled Artillery',
+    values: [{}, { value: 215, delta: 4, sources: ['http://example.org/2', 'http://example.org/3'] }],
+  },
 ];
 
-
-function valueAndDelta({ value, delta, sources }) {
-	return (value | 0) + (delta ? `(+${delta})` : '')
-}
+// function valueAndDelta({ value, delta, sources }) {
+//   return (value | 0) + (delta ? `(+${delta})` : '');
+// }
 
 export default function Dashboard({ data = mock, date }) {
-	// console.log('Dashboard', data)
-	return (
-		<div>
-			<AmmoTable data={data} date={date}/>
-			<table>
-				<tr>
-					<th />
-					<th>USA</th>
-					<th>Rest</th>
-				</tr>
-				{data.map(({ category, values }) => (
-					<tr key={category}>
-						<td>{category}</td>
-						<td>{valueAndDelta(values[0])}</td>
-						<td>{valueAndDelta(values[1])}</td>
-					</tr>
-				))}
-			</table>
-		</div>
-	);
+  // console.log('Dashboard', data)
+  return (
+    <div>
+      <AmmoTable data={data} date={date} />
+      {/*<table>*/}
+      {/*  <tr>*/}
+      {/*    <th />*/}
+      {/*    <th>USA</th>*/}
+      {/*    <th>Rest</th>*/}
+      {/*  </tr>*/}
+      {/*  {data.map(({ category, values }) => (*/}
+      {/*    <tr key={category}>*/}
+      {/*      <td>{category}</td>*/}
+      {/*      <td>{valueAndDelta(values[0])}</td>*/}
+      {/*      <td>{valueAndDelta(values[1])}</td>*/}
+      {/*    </tr>*/}
+      {/*  ))}*/}
+      {/*</table>*/}
+    </div>
+  );
 }
