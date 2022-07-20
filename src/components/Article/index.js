f53e683530fb065438459536654d197201084fe7
dbaranoff <cps.baranoff@gmail.com>
2022-07-20 14:37:35 +0300

    UI fixes and improvements:
    - added dummy data to blog article for testing
    - changes styles inside the Article component
    - removed SASS deprecated syntax

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,12 +1,13 @@
 import cn from 'classnames';
 import Markdown from 'markdown-to-jsx';
 import style from './style.scss';
+import { Fragment } from 'preact';
 
 const Index = ({ className, content }) => {
 	return content ? (
-		<div className={cn(className, style.container)}>
-			<Markdown>{content}</Markdown>
-		</div>
+		<article className={cn(className, style.container)}>
+			<Markdown options={{ wrapper: Fragment }}>{content}</Markdown>
+		</article>
 	) : null;
 };
 
