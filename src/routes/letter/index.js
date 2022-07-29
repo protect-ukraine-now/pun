7d0ff98234e4103d1acec15f547268f348efcd43
dbaranoff <cps.baranoff@gmail.com>
2022-07-29 21:19:51 +0300

    actions list moved under the h1

--- src/routes/letter/index.js
+++ src/routes/letter/index.js
@@ -25,11 +25,26 @@ export default function Letter(props) {
 			<section className={cn(style.section, style.banner)}>
 				<h1 className={style.title}>
 					TOGETHER WE CAN SAVE
-					<br /><span className={style.mobile}>&nbsp;</span>
+					<br/><span className={style.mobile}>&nbsp;</span>
 					UKRAINIAN LIVES!
 				</h1>
 			</section>
-			<section className={cn(style.section, style.parts)}>
+			<section className={cn(style.section, style.explanation)}>
+				<p>
+					Russia fires at will missiles and bombs into densely populated Ukrainian cities killing hundreds and
+					terrorizing
+					millions. Russia can do so with impunity as the Ukrainians don't have air defense systems capable of
+					intercepting such threats.
+				</p>
+				<p>
+					Despite what we have been led to believe, the US military aid is insufficient and needs to be substantially
+					increased. From the 20 billion of military assistance allocated by the Congress to be shipped by the end of
+					September less than 20% had been used. Hence, six US senators from both sides of the aisle had sent a request
+					to the Secretary of Defense and Chairman of the Joint Chiefs of Staff urging them to provide air defense
+					systems. Please read <a href="https://www.sullivan.senate.gov/imo/media/doc/20220715_Letter.pdf">their
+					letter</a>.
+				</p>
+				<h1 className={style.title}>SEND THE FOLLOWING LETTER TO YOUR REPRESENTATIVE IN U.S. CONGRESS</h1>
 				<ol className={style.actionsList}>
 					<li className={style.actionItem}>
 						Go to the {' '}
@@ -55,19 +70,6 @@ export default function Letter(props) {
 				</ol>
 			</section>
 			<section className={cn(style.section, style.letter)}>
-				<p>
-					Russia fires at will missiles and bombs into densely populated Ukrainian cities killing hundreds and terrorizing
-					millions. Russia can do so with impunity as the Ukrainians don't have air defense systems capable of
-					intercepting such threats.
-				</p>
-				<p>
-					Despite what we have been led to believe, the US military aid is insufficient and needs to be substantially
-					increased. From the 20 billion of military assistance allocated by the Congress to be shipped by the end of
-					September less than 20% had been used. Hence, six US senators from both sides of the aisle had sent a request
-					to the Secretary of Defense and Chairman of the Joint Chiefs of Staff urging them to provide air defense
-					systems. Please read <a href="https://www.sullivan.senate.gov/imo/media/doc/20220715_Letter.pdf">their letter</a>.
-				</p>
-				<h1 className={style.title}>SEND THE FOLLOWING LETTER TO YOUR REPRESENTATIVE IN U.S. CONGRESS</h1>
 				<div id="emailbody">
 					<p>
 						It’s been five months since the beginning of the illegal and unprovoked Russian attack on Ukraine. From the
@@ -76,14 +78,18 @@ export default function Letter(props) {
 						including civilians, women and children among them.
 					</p>
 					<p>
-						Six senators from both sides of the aisle including Richard Blumenthal and Lindsey O. Graham on July 15th have
-						addressed the Secretary of Defense to expedite military assistance for Ukraine. I urge you to join their ranks
-						and request the White House and DoD to immediately send more military equipment to Ukraine as appropriated by
+						Six senators from both sides of the aisle including Richard Blumenthal and Lindsey O. Graham on July 15th
+						have
+						addressed the Secretary of Defense to expedite military assistance for Ukraine. I urge you to join their
+						ranks
+						and request the White House and DoD to immediately send more military equipment to Ukraine as appropriated
+						by
 						the Congress. According to the group of senators, equipment needed for immediate delivery includes C-RAM
 						systems, fourth-generation fighter aircraft, larger and more capable unmanned aircraft systems (UAS).
 					</p>
 					<p>
-						According to Fareed Zakaria, who I tend to agree with, the appeasement of the totalitarian Russian regime will
+						According to Fareed Zakaria, who I tend to agree with, the appeasement of the totalitarian Russian regime
+						will
 						lead to an energy supply collapse during the coming winter and a likely annexation of Taiwan by China. In my
 						opinion, the military capabilities shipment to Ukraine is not only our moral obligation but a pragmatic
 						necessity to avoid a future world chaos.
@@ -96,7 +102,7 @@ export default function Letter(props) {
 			<section className={cn(style.section, style.social)}>
 				<h1>AFTER YOU ARE DONE PLEASE SHARE THIS SITE WITH YOUR FRIENDS, FAMILY AND ON SOCIAL NETWORKS</h1>
 				<div className={style.socialActions}>
-					<div className="sharethis-inline-share-buttons" />
+					<div className="sharethis-inline-share-buttons"/>
 				</div>
 			</section>
 		</Container>
