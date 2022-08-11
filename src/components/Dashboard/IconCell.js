96b32cffddc123b708d1d0f9241223616448eda0
Dima <25804461+dbaranoff@users.noreply.github.com>
2022-08-11 16:27:16 +0300

    New two columns table design (#9)
    * Table is split to left and right. 1st try
    
    * Hide the second header row on small screens
    
    * Table was made smaller
    
    * russia cell left padding increased
    
    * Header shadow, smaller icons
    
    * Smaller icons, smaller gap between tables

--- src/components/Dashboard/IconCell.js
+++ src/components/Dashboard/IconCell.js
@@ -1,5 +1,5 @@
 import cn from 'classnames';
-import { Text } from 'preact-i18n'
+import { Text, useText } from 'preact-i18n'
 import { AmmoIconMap } from './maps';
 import defaultIcon from '../../assets/icons/question_mark.svg';
 import style from './style.scss';
@@ -7,12 +7,11 @@ import style from './style.scss';
 const IconCell = ({ category }) => {
   const icon = AmmoIconMap[category] || defaultIcon;
 
+  const hint = useText(`weapon_category.${category}`);
+
   return (
     <div className={cn(style.category, style.cell)}>
-      <div className={style.icon} style={{ backgroundImage: `url("${icon}")` }} />
-      <div className={style.label}>
-        <Text id={`weapon_category.` + category}>{category}</Text>
-      </div>
+      <div className={style.icon} style={{ backgroundImage: `url("${icon}")` }} title={hint[category]} />
     </div>
   );
 };
--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,7 +1,7 @@
 import { Link } from 'preact-router';
-import { Text } from 'preact-i18n'
-import cn from 'classnames'
-import { IoMdSkipBackward, IoMdSkipForward } from 'react-icons/io'
+import { Text } from 'preact-i18n';
+import cn from 'classnames';
+import { IoMdSkipBackward, IoMdSkipForward } from 'react-icons/io';
 
 import style from './style.scss';
 import IconCell from './IconCell';
@@ -9,12 +9,14 @@ import DataCell from './DataCell';
 import Container from '../Container';
 import { useLanguage } from '../../tools/language';
 import { formatDate } from '../../tools/dates';
+import { Fragment } from 'preact';
+import { useCallback, useMemo } from 'preact/hooks';
 
 const approximateNumber = n => (
 	parseFloat(n)
-	? '>' + (Math.floor(parseFloat(n) / 1e3) * 1e3).toLocaleString()
-	: n
-)
+		? '>' + (Math.floor(parseFloat(n) / 1e3) * 1e3).toLocaleString()
+		: n
+);
 
 const Dashboard = (props) => {
 	// console.log('Dashboard', props)
@@ -22,9 +24,46 @@ const Dashboard = (props) => {
 
 	const formatter = formatDate(language);
 
-	let { from, till, prev, next, data } = props
+	let { from, till, prev, next, data } = props;
 	from = formatter(from);
 	till = formatter(till);
+
+	const left = data.slice(0, data.length / 2);
+	const right = data.slice(-data.length / 2);
+
+	const headLayout = useMemo(() => {
+		return (
+			<div className={cn(style.row, style.headRow)}>
+				<div className={style.head}/>
+				<div className={style.head}>
+					<Text id="report.usa">USA</Text>
+				</div>
+				<div className={style.head}>
+					<Text id="report.rest">Others</Text>
+				</div>
+				<div className={cn(style.head, style.russia)}>
+					<Text id="report.russia">Russia had</Text>
+				</div>
+			</div>
+		);
+	}, []);
+
+	const rowsRenderer = useCallback(({ category, values: [usa, rest, russia] }) => (
+		<div className={style.row}>
+			<IconCell category={category}/>
+			<DataCell className={style.valueCell} {...usa} key={`${category}-USA`}/>
+			<DataCell className={style.valueCell} {...rest} key={`${category}-rest`}/>
+			<DataCell className={cn(style.valueCell, style.russia)} {...{ value: approximateNumber(russia.value) }}
+								key={`${category}-russia`}/>
+		</div>
+	), []);
+
+	const renderTableLayout = useCallback((tableData) => (
+		<div className={style.table}>
+			{headLayout}
+			{tableData && tableData.length ? tableData.map(rowsRenderer) : null}
+		</div>
+	), [headLayout, rowsRenderer]);
 
 	return (
 		<Container>
@@ -55,29 +94,12 @@ const Dashboard = (props) => {
 					</div>
 				</div>
 			</h1>
-			<div className={style.table}>
-				<div className={style.head} />
-				<div className={style.head}>
-					<Text id="report.usa">USA</Text>
-				</div>
-				<div className={style.head}>
-					<Text id="report.rest">Others</Text>
-				</div>
-				<div className={cn(style.head, style.russia)}>
-					<Text id="report.russia">Russia had</Text>
-				</div>
 
-				{data.map(({ category, values: [usa, rest, russia] }) => (
-					<div className={style.row}>
-						<IconCell category={category} />
-						<DataCell className={style.valueCell} {...usa} key={`${category}-USA`} />
-						<DataCell className={style.valueCell} {...rest} key={`${category}-rest`} />
-						<DataCell className={cn(style.valueCell, style.russia)} {...{ value: approximateNumber(russia.value) }} key={`${category}-russia`} />
-					</div>
-				))}
+			<div className={style.splitter}>
+				{[left, right].map(renderTableLayout)}
 			</div>
 		</Container>
-	)
-}
+	);
+};
 
 export default Dashboard;
