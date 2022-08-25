8a2d9672946f87519bf2b8d18d4e5c48aca5e65d
dbaranoff <cps.baranoff@gmail.com>
2022-08-25 14:53:41 +0300

    Banner text changed, aligned; CTA btn hover restyled a bit

--- src/routes_/report/index.js
+++ src/routes_/report/index.js
@@ -7,25 +7,30 @@ import { Link } from 'preact-router';
 import { Text } from 'preact-i18n';
 import { useLanguage } from '../../tools/language';
 import reportBannerImage from '../../assets/images/banner-bg-1.png';
+import detectCountry from '../../tools/detectCountry';
 
 export default function Report(props) {
 	// console.log('Report props', props)
 	const language = useLanguage();
+	const country = detectCountry();
+
 	return (
 		<>
 			<Container className={style.container}>
 				<Dashboard {...props} />
 			</Container>
-			<Banner
-				className={style.banner}
-				title={<Text id="report.banner_title">Do You think Ukraine needs more weapon?</Text>}
-				image={reportBannerImage}
-				action={
-					<Link className={style.bannerBtn} path={`/${language}/letter`}>
-						<Text id="report.banner_btn">Help us, sign petition</Text>
-					</Link>
-				}
-			/>
+			{country !== 'US' && (
+				<Banner
+					className={style.banner}
+					title={<Text id="report.banner_title">Protect Ukraine now before it's too late!</Text>}
+					image={reportBannerImage}
+					action={
+						<Link className={style.bannerBtn} href={`/${language}/letter`}>
+							<Text id="report.banner_btn">Click here</Text>
+						</Link>
+					}
+				/>
+			)}
 			<Container className={style.chart}>
 				<AidChart {...props} />
 			</Container>
