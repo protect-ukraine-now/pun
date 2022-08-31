f30f5dd14c779c227bb11af7ddbb25270f7427f3
alexander-entin <alexander.entin@gmail.com>
2022-08-31 16:08:22 +0300

    build

--- src/data/prepare.js
+++ src/data/prepare.js
@@ -21,7 +21,7 @@ async function preparePages() {
 		title: 'US must arm Ukraine now, before it’s too late',
 		cover: 'https://protectukrainenow.org/assets/og.webp',
 		subtitle: `
-"With the necessary weapons and economic aid, Ukraine can defeat Russia. If it succeeds, our soldiers are less likely to have to risk their lives protecting U.S. treaty allies whom Russia also threatens."
+«With the necessary weapons and economic aid, Ukraine can defeat Russia. If it succeeds, our soldiers are less likely to have to risk their lives protecting U.S. treaty allies whom Russia also threatens»
 
 This is a public opinion of US 20 top national security experts.
 		`,
