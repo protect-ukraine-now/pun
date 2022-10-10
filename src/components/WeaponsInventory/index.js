3d7a79284ba291b3536e579f0c928b5907e61a3d
alexander-entin <alexander.entin@gmail.com>
2022-10-10 22:01:57 +0300

    inventory table header translation

--- src/components/WeaponsInventory/index.js
+++ src/components/WeaponsInventory/index.js
@@ -1,20 +1,18 @@
 import { Text } from 'preact-i18n'
 
-import style from './style.scss';
+import style from './style.scss'
 import { latestReport, inventoryReport } from '../../data/report'
 import { formatDate } from '../../tools/date'
 import WeaponsTable from '../WeaponsTable'
 
 let data = inventoryReport()
 
-console.log(data)
-
 export default function WeaponsInventory({ language, className }) {
     const formatter = formatDate(language)
     let till = formatter(latestReport.till)
     let title = <Text id="inventory.title" fields={{ till }} />
     let subtitle = <div className={style.subtitle}><Text id="inventory.subtitle" fields={{ till }} /></div>
-    let head = ["Supply", "PDA"]
+    let head = [<Text id="inventory.possessed" />, <Text id="inventory.pda" />]
     let description = <Text id="inventory.description" />
     return <WeaponsTable {...{ title, subtitle, head, data, description, className }} />
 }
