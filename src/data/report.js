97a22d20295d2b47cde8ce51e4e58b44844cad43
dbaranoff <cps.baranoff@gmail.com>
2022-11-20 15:33:12 +0200

    latest date changed according to the mew release date

--- src/data/report.js
+++ src/data/report.js
@@ -3,7 +3,7 @@ import commits from './commits.json'
 import { isoDate, DAY } from '../tools/date'
 
 let first = '2022-07-17'
-let latest = '2022-11-06'
+let latest = '2022-11-20'
 
 let timespan = 14 * DAY
 export let Report = till => {
