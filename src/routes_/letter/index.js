dc51032b79b00c6bbde22b067f90968696048c60
alexander-entin <alexander.entin@gmail.com>
2022-12-30 02:18:39 +0200

    letter motivation

--- src/routes_/letter/index.js
+++ src/routes_/letter/index.js
@@ -1,5 +1,7 @@
 import cn from 'classnames';
 import Helmet from 'preact-helmet';
+
+import Article from '../../components/Article'
 import detectCountry from '../../tools/detectCountry';
 import Container from '../../components/Container';
 import style from './style.scss';
@@ -18,25 +20,25 @@ export default function Letter() {
 		<Container className={style.container}>
 			<section className={cn(style.section, style.banner)}>
 				<h2 className={style.title}>
-					US must arm Ukraine now
+					Save Ukrainian lives with your Petition!
 					<br /><span className={style.mobile}>&nbsp;</span>
-					before it’s too late
 				</h2>
 			</section>
 			<section className={cn(style.section, style.explanation)}>
-				<p>
-					Russia launched the most unprecedented missile attack across the entire Ukraine targeting critical infrastructure and slaughtering civilians. More than 3600 missiles have been launched on Ukrainian territory. By October, tens of thousands of civilians had been killed, including more than 400 children.
-				</p>
-				<p>
-					As of now not a single air defense system was provided by the US. There is no excuse for this cowardly and heartless position of the White House. Empty pledges and condemnations have not saved a single killed child.
-				</p>
-				<p>
-					Significantly MORE MILITARY AID is needed urgently. Time to act is NOW before more children are killed.​
-				</p>
+				<Article>
+					<ul>
+						<li>Innocent people are killed in their homes every day; on Christmas Eve 17 civilians were killed and 71 were wounded</li>
+						<li>The prolonged war in Ukraine will shatter the world stability; we can’t allow the war to evolve into a multiyear conflict</li>
+						<li>General Zaloujny is ready to end this war:
+							<blockquote>
+							“I know that I can beat this enemy. But I need resources. I need 300 tanks, 600-700 IFVs, 500 Howitzers”
+							</blockquote>
+						</li>
+					</ul>
+				</Article>
 				{country && country !== 'US' &&
-					<h2>If you are a US citizen (and have a US address)</h2>
+					<h2><br />If you are a US citizen (and have a US address)</h2>
 				}
-
 				<div id='can-letter-area-protect-ukraine-now' style='width: 100%'></div>
 			</section>
 		</Container>
