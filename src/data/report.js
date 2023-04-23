cb65fab69587e54a768c980671431115b0f4a1d9
alexander-entin <alexander.entin@gmail.com>
2023-04-23 19:32:53 +0300

    publish

--- src/data/report.js
+++ src/data/report.js
@@ -3,7 +3,7 @@ import commits from './commits.json'
 import { isoDate, DAY } from '../tools/date'
 
 const first = '2022-07-17'
-export const latest = '2023-04-09'
+export const latest = '2023-04-23'
 
 let timespan = 14 * DAY
 export let Report = till => {
