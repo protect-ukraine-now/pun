d3bad5b69ac214fcfc1fb4c2347ba079dbaa7e0f
alexander-entin <alexander.entin@gmail.com>
2023-05-26 19:59:51 +0300

    style

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,7 +2,7 @@ import { Head, Layout } from 'rakkasjs'
 import 'reset-css'
 
 import 'src/style/index.module.scss'
-import { useLanguage } from 'src/tools/language'
+import { useLanguage, useText } from 'src/tools/language'
 import logo1 from 'src/assets/logo/logo-horizontal-light.svg'
 import logo2 from 'src/assets/logo/logo-dark-horizontal.svg'
 import Header from 'src/components/Header'
@@ -36,11 +36,28 @@ const config = {
 
 const MainLayout: Layout = ({ children }) => {
 	const language = useLanguage()
+	const text = useText()
 	const { header, footer } = config[useApp()]
 	return <>
 		<Head prioritizeSeoTags>
 			<html lang={language} />
-			<title lang="en">Protect Ukraine NOW</title>
+			<title lang="en">{text('seo.title')}</title>
+			<meta property="description" content={text('seo.description')} />
+
+			<meta property="og:title" content={text('seo.title')} />
+			<meta property="og:description" content={text('seo.description')} />
+			<meta property="og:image" content={text('seo.image')} />
+
+			<meta name="twitter:card" content="summary_large_image" />
+
+			<meta charSet="utf-8" />
+			<meta name="viewport" content="width=device-width,initial-scale=1" />
+			<meta name="mobile-web-app-capable" content="yes" />
+			<meta name="apple-mobile-web-app-capable" content="yes" />
+
+			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+			<script>{`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TSJ66R2');`}</script>
 		</Head>
 		<Header {...header} />
 		{children}
