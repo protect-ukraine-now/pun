56711a0549c37ea15de547e7216191ebebd459fc
alexander-entin <alexander.entin@gmail.com>
2023-11-06 14:13:57 +0200

    scroll to top on navigation

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -56,9 +56,10 @@ const MainLayout: Layout = ({ children, url }) => {
 			<meta name="mobile-web-app-capable" content="yes" />
 			<meta name="apple-mobile-web-app-capable" content="yes" />
 
+			{/* <link rel="stylesheet" href="https://unpkg.com/reset-css/reset.css" /> */}
 			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-			<script>{`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TSJ66R2');`}</script>
+			<script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TSJ66R2');`}</script>
 		</Head>
 		<Header {...header} />
 		{children}
