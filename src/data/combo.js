55c244f438cba42e24739b339b650c4a9adc3156
alexander-entin <alexander.entin@gmail.com>
2024-02-25 18:08:15 +0200

    publish

---
+++ src/data/combo.js
@@ -0,0 +1,54 @@
+import fs, { writeFileSync } from 'fs'
+import { indexBy } from 'rambda'
+import dayjs from 'dayjs'
+
+import balance from './balance.json' with { type: 'json' }
+import commits from './commits.json' with { type: 'json' }
+import oryx from './oryx.json' with { type: 'json' }
+
+const vs = {
+	ru: 'ua',
+	ua: 'ru',
+}
+
+const file = `combo.tsv`
+
+let balance2 = balance.flatMap(b => Object.keys(vs).map(country => {
+	const x = b.byModel.reduce((a, v) => {
+		let x = ~v.model.indexOf('(in store)') ? 0 : v[country] || 0
+		console.log(b.category, country, v.model, v[country], x)
+		return a + x
+	}, 0)
+	console.log(['2022-01', b.category, country, x])
+	return ['2022-01', b.category, country, x]
+}))
+
+commits.forEach(([date, country, category, model, qty]) => {
+	const month = date.substr(0, 7)
+	oryx[month][category].ua.provided = (oryx[month][category].ua.provided || 0) + qty
+})
+
+let prev
+Object.values(oryx).forEach(x => {
+	Object.entries(x).forEach(([category, { ru, ua }]) => {
+		if (category === 'Tank') console.log(prev?.[category])
+		ru.lost2 = Math.max(0, ru.lost - (prev?.[category].ru.lost || 0))
+		ua.lost2 = Math.max(0, ua.lost - (prev?.[category].ua.lost || 0))
+		ru.captured2 = Math.max(0, ua.captured - (prev?.[category].ua.captured || 0))
+		ua.captured2 = Math.max(0, ru.captured - (prev?.[category].ru.captured || 0))
+    })
+	prev = x
+})
+
+let combo = Object.entries(oryx).flatMap(([month, x]) =>
+	Object.entries(x).flatMap(([category, y]) =>
+		Object.entries(y).map(([country, z]) =>
+			[month, category, country, , -z.lost2, z.captured2, z.provided]
+		)
+	)
+)
+
+const header = 'month\tcategory\tcountry\thad\tlost\tcaptured\tprovided\n'
+const rows = [...balance2, ...combo].map(x => x.join('\t')).join('\n')
+writeFileSync(file, header + rows)
+
--- src/data/load-oryx.js
+++ src/data/load-oryx.js
@@ -1,62 +1,74 @@
 import fs from 'fs'
 import dayjs from 'dayjs'
+import { map } from 'rambda'
 
-const what = [
-	'Towed Artillery',
-	'Self-Propelled Artillery',
-	'Multiple Rocket Launchers',
-	'Self-Propelled Anti-Aircraft Guns',
-	'Surface-To-Air Missile Systems',
-	'Aircraft',
-	'Helicopters',
-	'Tanks',
-	'Infantry Fighting Vehicles',
-]
+import oryx from './oryx.json' with { type: 'json' }
+
+const what = {
+	'Towed Artillery': 'Towed Artillery',
+	'Self-Propelled Artillery': 'Self-Propelled Artillery',
+	'Multiple Rocket Launchers': 'Multiple Launch Rocket System',
+	'Self-Propelled Anti-Aircraft Guns': 'Air Defense System',
+	'Surface-To-Air Missile Systems': 'Air Defense System',
+	'Aircraft': 'Warplane',
+	'Helicopters': 'Helicopter',
+	'Tanks': 'Tank',
+	'Infantry Fighting Vehicles': 'Infantry Fighting Vehicle',
+}
 const urls = {
 	ru: 'https://web.archive.org/web/${date}/https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html',
 	ua: 'https://web.archive.org/web/${date}/https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-ukrainian.html',
 }
-const file = `oryx.tsv`
+const countries = {
+	ru: 'Russia',
+	ua: 'Ukraine',
+}
+const file = `oryx.json`
+const empty = Object.fromEntries(Object.values(what).map(what =>
+	[what, map(_ => ({ lost: 0, captured: 0 }), countries)]
+))
 
-let data = fs.readFileSync(file, 'utf8').split('\n').map(x => x.split('\t'))
-let total = data.slice(1).reduce((total, row) => {
-	let [country] = row
-	total[country] = row.slice(2).map((x, i) => (total[country][i] ?? 0) + parseInt(x))
-	return total
-}, { ru: [], ua: [] })
-// console.log(total)
-let [_, date] = data[data.length - 2]
-
-function extract(page, what) {
-	what = `>${what}`
-	let i = page.indexOf(what) + what.length
-	i = page.indexOf('(', i) + 1
-	let j = page.indexOf(',', i)
-	let x = page.substring(i, j)
-	// console.log(what, i, j, x.substring(0, 40))
-	return parseInt(x)
+function extract(page, what, country) {
+	const re = new RegExp(`>${what}.*\\((\\d+),[^)]*?(?:captured: (\\d+))?\\)`)
+	const i = page.indexOf(`${countries[country]} - `)
+	const match = page.slice(i).match(re)
+	if (!match) {
+		console.error(`NO MATCH: ${what}`)
+	}
+	const [_, lost, captured] = match || [0, 0, 0]
+	// console.log(what, { lost, captured })
+	return [parseInt(lost), parseInt(captured) || 0]
 }
 
+const dates = Object.keys(oryx)
+let date = dates[dates.length - 1] || '2022-01-01'
 const today = dayjs().format('YYYY-MM-DD')
-for(;;) {
+for (;;) {
 	date = dayjs(date).add(1, 'M').endOf('M').format('YYYY-MM-DD')
 	console.log(date)
 	if (date >= today) break
+	const current = structuredClone(empty)
 	for (const country in urls) {
-		const url = urls[country].replace('${date}', date.replace(/-/g, ''))
-		// console.log(url)
+		let url = urls[date < '2022-04-20' ? 'ru' : country]
+		url = url.replace('${date}', date.replace(/-/g, ''))
+		console.log(url)
 		const page = await fetch(url, {
 			redirect: 'follow',
 			follow: 9,
 		}).then(x => x.text())
-		const current = what.map((what, i) => {
-			let x = Math.max(0, extract(page, what) - total[country][i])
-			total[country][i] += x
-			return x
-		})
-		const row = [country, date.substring(0, 7), ...current].join('\t')
-		console.log(row)
-		fs.appendFileSync(file, row + '\n')
 		// fs.writeFileSync('oryx.html', page)
+		const rows = Object.entries(what).forEach(([what, category]) => {
+			let [lost, captured] = extract(page, what, country)
+			console.log(country, what, lost, captured)
+			current[category][country].lost += +lost
+			current[category][country].captured += +captured
+		})
 	}
+	const rows = Object.entries(current).map(([what, x]) =>
+		Object.entries(x).map(([country, { lost, captured }]) =>
+			[date.substring(0, 7), what, country, lost, captured].join('\t') + '\n'
+		).join('')
+	).join('')
+	oryx[date.substring(0, 7)] = current
+	fs.writeFileSync(file, JSON.stringify(oryx, null, '\t'))
 }
