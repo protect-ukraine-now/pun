044912cd5dbc411d506125d7349a4f25fd987249
alexander-entin <alexander.entin@gmail.com>
2024-05-18 16:55:38 +0300

    cms experiment

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,4 +1,5 @@
 import { config, fields, collection } from '@keystatic/core';
+import Layout from '@src/layouts/Layout.astro';
 
 export default config({
 	storage: {
@@ -6,34 +7,48 @@ export default config({
 	},
 	collections: {
 		notes: collection({
-			label: 'notes',
-			slugField: 'title',
+			label: 'Notes',
 			path: 'src/content/notes/*',
+			slugField: 'title',
 			format: { contentField: 'content' },
 			schema: {
 				title: fields.slug({ name: { label: 'Title' } }),
-				content: fields.document({
+				content: fields.markdoc({
 					label: 'Content',
-					formatting: true,
-					dividers: true,
-					links: true,
-					images: true,
+					extension: 'md',
 				}),
 			},
 		}),
 		pages: collection({
-			label: 'pages',
+			label: 'Pages',
 			slugField: 'title',
-			path: 'src/pages/*',
+			path: 'src/content/pages/*',
 			format: { contentField: 'content' },
 			schema: {
 				title: fields.slug({ name: { label: 'Title' } }),
-				content: fields.document({
+				content: fields.markdoc({
 					label: 'Content',
-					formatting: true,
-					dividers: true,
-					links: true,
-					images: true,
+				}),
+			},
+		}),
+		pagesEn: collection({
+			label: 'Pages EN',
+			slugField: 'title',
+			path: 'src/pages/en/*',
+			format: { contentField: 'content' },
+			schema: {
+				title: fields.slug({ name: { label: 'Title' } }),
+				layout: fields.text({
+					label: 'Layout',
+					default: '@layouts/Layout.astro',
+				}),
+				content: fields.markdoc({
+					label: 'Content',
+					extension: 'mdx',
+					image: {
+						directory: 'src/assets',
+						publicPath: '@assets'
+					},
 				}),
 			},
 		}),
