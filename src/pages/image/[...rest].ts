b46385f57d6fdace1df554d13f54cbe3cfd7b338
alexander-entin <alexander.entin@gmail.com>
2024-06-10 21:05:44 +0300

    cache experiment

--- src/pages/image/[...rest].ts
+++ src/pages/image/[...rest].ts
@@ -3,7 +3,7 @@ export function GET({ request }) {
 	const height = 627
 	const header = 77
 	const crop = `0,${header},${width},${height}`
-	const ttl = 600
+	const ttl = 601
 	const page = request.url.replace('/image', '').replace('/localhost:4321/', '/protectukrainenow.org/')
 	const url = `https://api.apiflash.com/v1/urltoimage?url=${page}&width=${width}&height=${header+height}&ttl=${ttl}&crop=${crop}&format=webp&access_key=c94952bab8a34be1a401fb0cd1ec60dd`
 	return fetch(url, {
