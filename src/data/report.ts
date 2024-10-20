899428dcbfc23accb3b3fb03f0a6fdfb9228b7f7
alexander-entin <alexander.entin@gmail.com>
2024-10-20 18:46:09 +0300

    publish

--- src/data/report.ts
+++ src/data/report.ts
@@ -6,7 +6,7 @@ import categories from './categories.json'
 import { isoDate, DAY } from '../tools/date'
 
 const first = '2022-07-17'
-export const latest = '2024-10-06'
+export const latest = '2024-10-20'
 
 let timespan = 14 * DAY
 export let Report = till => {
