08fa9196e7e84ce428d052137390f8b510d54aa3
alexander-entin <alexander.entin@gmail.com>
2025-01-03 18:12:14 +0200

    publish

--- src/components/LossesChart/LossesChart.tsx
+++ src/components/LossesChart/LossesChart.tsx
@@ -1,8 +1,6 @@
 import Vega from '@components/Vega/Vega'
 import spec from '@data/losses-w-data.vg.json'
 
-console.log('losses', spec)
-
 export function LossesChart() {
 	return (
 		<Vega spec={spec} />
