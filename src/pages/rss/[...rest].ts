1ae41d7aa7d5cd53da6995e1986b335a2bc09633
alexander-entin <alexander.entin@gmail.com>
2025-02-01 13:06:04 +0200

    rss

--- src/pages/rss/[...rest].ts
+++ src/pages/rss/[...rest].ts
@@ -1,4 +1,4 @@
-import { xml2json } from 'xml-js'
+import xml2json from 'simple-xml-to-json'
 
 export async function GET({ request }) {
 	let query = request.url.split('/rss/')[1] ?? ''
@@ -7,13 +7,15 @@ export async function GET({ request }) {
 	const rssUrl = `https://news.google.com/rss/` + query
 	console.log(rssUrl)
 	let xml = await (await fetch(rssUrl)).text()
-	console.log(xml)
+	// console.log(xml)
 	try {
-		let feed = JSON.parse(xml2json(xml, { compact: true }))
-		console.log(feed)
-		let items = feed.rss.channel.item.map(({ title, link, source }) => ({
-			title: title._text,
-			link: link._text,
+		let feed = xml2json.convertXML(xml)
+		// console.log(JSON.stringify(feed, null, 4))
+		let items = feed.rss.children[0].channel.children.map(x => x.item).filter(x => x)
+		console.log(JSON.stringify(items, null, 4))
+		items = items.map(i => ({
+			title: i.children[0].title.content,
+			link: i.children[1].link.content,
 		}))
 		return new Response(JSON.stringify(items, null, '	'))
 	} catch(e) {
